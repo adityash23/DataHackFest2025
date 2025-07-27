a1d34c2d41df9640349dbd14bf2354ea594e860c
adityash23 <aditya3242@gmail.com>
2025-07-27 01:40:40 -0400

    app updated to show user-chosen currency exchange rate plot

--- app.py
+++ app.py
@@ -2,19 +2,34 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import random
+import altair as alt
 
 # Code below to be moved to a new folder to modularize it
 def stats(df):
     st.header("General Statistics")
     st.write(df.describe())
 
-def genres(df):
-    st.header("Genres")
-    fix, ax = plt.subplots(1, 1)
-    ax.scatter(df['genres'], df['emotions'], alpha=0.5)
-    ax.set_xlabel('Genres')
-    ax.set_ylabel('Emotions')
-    st.pyplot(fix)
+def exchange(df):
+    df['date'] = pd.to_datetime(df['date'])
+        
+    # allow user to select currency to plot
+    currencies = df['currency'].unique().tolist()
+    selected = st.multiselect("Select currencies", currencies, default=currencies[0]) # only select first currency if user choose multiple
+
+    df_filtered = df[df['currency'].isin(selected)]
+
+    # altair chart
+    chart = alt.Chart(df_filtered).mark_line().encode(
+        x='date:T',
+        y=alt.Y('exchange_rate:Q', scale=alt.Scale(domain=[1.2, 1.8])),
+        color='currency:N'
+    ).properties(
+        width=900,
+        height=400,
+        title="Currency Exchange Rate Over Time"
+    )
+
+    st.altair_chart(chart, use_container_width=True)
 
 def emotions(df):
     st.header("Emotions")
@@ -27,41 +42,24 @@ def emotions(df):
     st.pyplot(fig)
 
 
-st.title("Data HackFest 2025")
-st.text("This is a emotion based music playlist generator app with multiple customization options.")
+st.title("DataHackFest 2025")
+st.text("This is a forex rate predictor with news sentiment analysis")
 
 st.sidebar.title("Nav")
 
 uploaded_file = st.sidebar.file_uploader("Upload a custom csv file with your personal music emotions", type=["csv"])
 
-options = st.sidebar.radio("View the dataset:", ("Home", "General statistics", "Genres", "Emotions"))
+options = st.sidebar.radio("View the dataset:", ("Home", "General statistics", "Exchange Rates", "Emotions"))
 
 # datasets and page setup
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
 else:
-    df = pd.read_csv("data/music.csv")
+    df = pd.read_csv("data/forex/filtered_forex_rates.csv")
 
 if options == "General statistics":
     stats(df)
-elif options == "Genres":
-    genres(df)
+elif options == "Exchange Rates":
+    exchange(df)
 elif options == "Emotions":
     emotions(df)
-
-# user input
-user_input = st.text_input("Enter your vibe today", placeholder="Type the emotion here...")
-
-if user_input:
-    st.write(f"Great! Today's vibe is - {user_input}!")
-
-output_bool = random.choice([True, False])  # temp model prediction
-
-while not output_bool:
-    st.write("Generating your playlist... Please wait and explore the dataset meanwhile")  
-    output_bool = random.choice([True, False])   
-    if output_bool:
-        break
-
-#st.experimental_rerun()
-st.write("Your playlist is generated successfully!")
